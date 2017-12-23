588f6f4f6aca245ceb3a64ca7a2f741c783c019b
cimuncang <30565858+cimuncang@users.noreply.github.com>
2017-12-23 21:42:12 +0700

    Add files via upload

---
+++ config/restart-dropbear.sh
@@ -0,0 +1,6 @@
+            echo "Servie dropbear restart .................tunggu sebentar"
+            sleep 0.3
+            service dropbear restart
+            echo ""
+	    echo "Script Created by http://www.sshinjector.net"
+            echo "Terimakasih sudah berlanggan di sshinjector.net"
---
+++ config/restart-openvpn.sh
@@ -0,0 +1,6 @@
+            echo "Servie openvpn restart .................tunggu sebentar"
+            sleep 0.3
+            service openvpn restart
+            echo ""
+	    echo "Script Created by http://www.sshinjector.net"
+            echo "Terimakasih sudah berlanggan di sshinjector.net"
